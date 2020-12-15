aff162ac61690d23e5ad42b6314adbe6cd8df58b
kazukioyama <kazuki.oy03@gmail.com>
2020-12-15 19:37:31 +0900

    #14

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import Basic2 from './components/Basic2'
+// import BasicUseEffect from './components/BasicUseEffect'
+import TimerContainer from './components/TimerContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <Basic2/> */}
+        <TimerContainer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
